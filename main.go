5f5c82dc3c8fb626ecff1a0059e0ce003264603d
Ian Macalinao <me@ian.pw>
2016-01-23 15:38:12 -0800

    feat: add termbox

--- main.go
+++ main.go
@@ -1,17 +1,38 @@
 package main
 
-import (
-	"net/http"
+import "github.com/nsf/termbox-go"
 
-	"github.com/gorilla/mux"
-	"github.com/simplyianm/timehub/handlers"
-)
+func switch_page(dir int) {
+}
+
+func draw_all() {
+}
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.HomeHandler)
-	go func() {
-		http.ListenAndServe(":8000", r)
-	}()
-	panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("static/"))))
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	draw_all()
+loop:
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyEsc:
+				break loop
+			case termbox.KeyArrowDown, termbox.KeyArrowRight:
+				switch_page(1)
+				draw_all()
+			case termbox.KeyArrowUp, termbox.KeyArrowLeft:
+				switch_page(-1)
+				draw_all()
+			}
+		case termbox.EventResize:
+			draw_all()
+		}
+	}
+
 }
