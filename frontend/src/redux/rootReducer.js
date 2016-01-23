1d7a1da86cbb0cd232c17c250054ad5b2c7244a0
Ian Macalinao <me@ian.pw>
2016-01-23 15:31:07 -0800

    Revert "Revert "Revert "fix: add scrollspy component"""
    This reverts commit 33bf0cc1f1be4f81de6f85aa7dd4f0fdcec4088f.

--- frontend/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { routeReducer as router } from 'redux-simple-router'
-import counter from './modules/counter'
-
-export default combineReducers({
-  counter,
-  router
-})
